5f9258494ae479842d8c4e69dd2da7cfca084312
Leonardo Ramos <oliveirarleo@gmail.com>
2022-04-18 14:29:14 -0300

    fix: pathfind build on colab

--- src/quoridor/pathfind/build.sh
+++ src/quoridor/pathfind/build.sh
@@ -2,6 +2,6 @@ rm -rf build
 mkdir build
 cd build
 #cmake ..
-cmake -DCMAKE_PREFIX_PATH=/usr/local/lib/python3.6/dist-packages/ ..
+cmake -DCMAKE_PREFIX_PATH=/usr/local/lib/python3.7/dist-packages/ ..
 make
 cd ..
